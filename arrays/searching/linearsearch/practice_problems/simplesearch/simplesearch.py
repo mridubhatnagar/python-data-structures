20321841c6beb6d5383a895fa6fab0919bde83ed
mridubhatnagar <mridubhatnagar@rediffmail.com>
2019-12-15 20:37:34 +0530

    implementation of simple search problem from hackerearth's linear search section.

---
+++ arrays/searching/linearsearch/practice_problems/simplesearch/simplesearch.py
@@ -0,0 +1,28 @@
+"""
+Problem Statement - https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/simple-search-4/
+"""
+
+'''
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+'''
+
+# Write your code here
+L=[]
+total_integers=int(input())
+numbers=input()
+L = numbers.split()
+search_element=int(input())
+    
+for element in enumerate(L):
+    if int(element[1])==int(search_element):
+        result = element[0]
+        break
+    else:
+        result = -1
+print(result)
+    
